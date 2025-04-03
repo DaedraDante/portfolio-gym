4b1e93a5a270d68353d3e7bb3a5c45962cff5cc6
DaedraDante <153109749+DaedraDante@users.noreply.github.com>
2025-04-03 20:27:17 +0300

    Update 3 , benefits section essentials
    i added the headers and paragraphs of the benefits sections ,I need to find some icons to match them and then style , mmm cant wait to style for responsivity too

--- portfolio-gym-project/src/FeaturesSection.jsx
+++ portfolio-gym-project/src/FeaturesSection.jsx
@@ -3,6 +3,35 @@ function FeaturesSection() {
     return(
             <div className="features-section">
                 <h1>This is the features section</h1>
+                <h2>We only provide whats best for our customers</h2>
+                <div className="features-benefits-section">
+                    <div>
+                        <h3>24/7 Acces</h3>
+                        <p>We offer you the choice to come anytime to come to our gym
+                            and spend time building that temple-like body
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Sauna Service</h3>
+                        <p>We provide sauna services for both men and women so our
+                           customers can properly rejuvenate and sooth the muscles
+                           along with your spirit in this peaceful environment
+                        </p>
+                    </div>
+                    <div>
+                        <h3>State of the art equipment</h3>
+                        <p>We only provide the best of the best workout equipment
+                            for our customers
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Professional Personal Trainers</h3>
+                        <p>We have an array of skilled personal trainers ready to be
+                            guidiing you across the process of shaping your own
+                            greek statue-like body
+                        </p>
+                    </div>
+                </div>
             </div>
     )
 }
