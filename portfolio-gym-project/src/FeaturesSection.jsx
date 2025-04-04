2b100730b20229522a4d2d94deac57b01d637367
DaedraDante <153109749+DaedraDante@users.noreply.github.com>
2025-04-04 15:59:29 +0300

    Update 5 , benefits section done

--- portfolio-gym-project/src/FeaturesSection.jsx
+++ portfolio-gym-project/src/FeaturesSection.jsx
@@ -2,33 +2,39 @@
 function FeaturesSection() {
     return(
             <div className="features-section">
-                <h1>This is the features section</h1>
-                <h2>We only provide whats best for our customers</h2>
+                {/* <h1>This is the features section</h1> */}
+                <h2 style={{textAlign:"center",fontSize:"2.5rem",marginTop:"6rem"}}>What we provide at Zeus Fit  gym</h2>
+                <p style={{textAlign:"center",marginTop:"1rem"}}>Our main goal is to ensure the customers that 
+                    their experience here is the best of the best and <br></br>they can reach their peak potential
+                    at any time of the day with the best equipment,staff and rejuvenating mediums
+                </p>
                 <div className="features-benefits-section">
-                    <div>
-                        <img src="\icons8-sauna-50.png" alt="sauna icon" />
+                    <div className="features-benefit-div">
+                        <img src="/schedule_24dp_33CDD8_FILL0_wght400_GRAD0_opsz24.png" alt="sauna icon" />
                         <h3>24/7 Acces</h3>
-                        <p>We offer you the choice to come anytime to come to our gym
-                            and spend time building that temple-like body
+                        <p>We offer you the choice to build your own statue-like body at any time interval you so desire to
                         </p>
                     </div>
-                    <div>
+                    <div className="features-benefit-div">
+                        <img src="/self_improvement_24dp_33CDD8_FILL0_wght400_GRAD0_opsz24.png" alt="time icon" /> 
                         <h3>Sauna Service</h3>
                         <p>We provide sauna services for both men and women so our
                            customers can properly rejuvenate and sooth the muscles
                            along with your spirit in this peaceful environment
                         </p>
                     </div>
-                    <div>
+                    <div className="features-benefit-div">
+                        <img src="/fitness_center_24dp_33CDD8_FILL0_wght400_GRAD0_opsz24.png" alt="equipment icon" /> 
                         <h3>State of the art equipment</h3>
-                        <p>We only provide the best of the best workout equipment
+                        <p>We only provide the best of the best top grade workout equipment
                             for our customers
                         </p>
                     </div>
-                    <div>
+                    <div className="features-benefit-div">
+                        <img src="/handshake_24dp_33CDD8_FILL0_wght400_GRAD0_opsz24.png" alt="trainer icon" />
                         <h3>Professional Personal Trainers</h3>
                         <p>We have an array of skilled personal trainers ready to be
-                            guidiing you across the process of shaping your own
+                            guiding you across the process of shaping your own
                             greek statue-like body
                         </p>
                     </div>
--- portfolio-gym-project/src/QuoteSection.jsx
+++ portfolio-gym-project/src/QuoteSection.jsx
@@ -3,7 +3,6 @@ function QuoteSection() {
     return(
         <div className="quote-section">
             <h1>Join our pantheon and see results</h1>
-            <hr></hr>
         </div>
     )
 }
