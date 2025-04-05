4da7112a282b71e4fa95b46344d4aad4b06a4701
DaedraDante <153109749+DaedraDante@users.noreply.github.com>
2025-04-05 15:24:01 +0300

    Update 6 , started testimonials section
    I layed out the base for the testimonials section ,right now its v e r y ugly but I look forward to make it look D E C E N T.

--- portfolio-gym-project/src/TestimonialsSection.jsx
+++ portfolio-gym-project/src/TestimonialsSection.jsx
@@ -3,7 +3,31 @@ function TestimonialsSection() {
     return (
         <div className="testimonials-section">
             <h1>This is the testimonials section</h1>
-        </div>  
+                <div className="testimonials-section-div-for-pics">
+                    <div className="testimonial-section-individual-div">
+                    <img src="https://i0.wp.com/thecutgym.com/wp-content/uploads/2024/06/male-body-transformation-london-2.webp?fit=1000%2C700&ssl=1" alt="before-after-pic-1" />
+                    <p>Initially I started because I wanted to train 
+                         for my basketball team but then I fell in love
+                         with working out here at Zeus Fit Gym.
+                    </p>
+                    </div>
+                    <div className="testimonial-section-individual-div">
+                    <img src="https://i0.wp.com/thecutgym.com/wp-content/uploads/2024/06/male-body-transformation-london-1-1.webp?fit=1000%2C700&ssl=1" alt="before-after-pic-2" />
+                    <p>Starting out at this age is hard for some but thanks to the 
+                        kind staff at Zeus Fit Gym I have been properly trained, that sauna is great too!
+                    
+                    </p>
+                    </div>
+                    <div className="testimonial-section-individual-div">
+                    <img src="https://i0.wp.com/thecutgym.com/wp-content/uploads/2024/05/body-transformation-male.webp?fit=1000%2C700&ssl=1" alt="before-after-pic-3" />
+                    <p> I hated being bloated and bulky all the time and decided to finally make a change,
+                        thankfully due to the conditions here at Zeus Fit Gym , I managed to take the leap and win.
+                    </p>
+                    </div>  
+                </div>
+            </div>  
+            
+            
     )
 }
 
